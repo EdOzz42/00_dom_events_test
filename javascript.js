a21c6567638ac0bb3551492e40b42a32fe961a06
Eduardo de Oliveira <eduardoolive133@gmail.com>
2022-10-23 03:50:26 -0300

    Aprendendo...

---
+++ javascript.js
@@ -0,0 +1,11 @@
+var p1 = window.document.getElementsByTagName('p')[0]
+p1.style.background = 'black'
+p1.innerText = 'O resultado estará aqui em baixo'
+
+/*var div = window.document.getElementById('caq')
+div.style.fontFamily = 'Comic Sans MS'
+div.style.color = 'aquamarine'*/
+
+/*var div = window.document.querySelector('div#caq')*/
+var div = window.document.querySelector('div.caq')
+div.style.background = 'turquoise'
